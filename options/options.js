db8b7aefc02dd315ef8200d258d1cc5508102177
Crowdbotics <team@crowdbotics.com>
2022-03-21 22:52:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "serenity_cleaners_34351",
+  url: "https://serenity_cleaners_34351.botics.co",
+  api: "https://serenity_cleaners_34351.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
